09532b331d514549d48cd7910d0a4255ce2c5e2b
ryangamblee <ryang1991@live.com>
2016-03-01 11:58:32 +0000

    Added files via upload

---
+++ app.js
@@ -0,0 +1,1 @@
+angular.module('com533',['ui.bootstrap']);
---
+++ scripts/controller.js
@@ -0,0 +1,26 @@
+angular
+    .module('com533')
+    .controller('com533Controller', function ($scope) {
+    
+    $scope.football = [
+    {
+        "team": "Spurs",
+        "players":Harry Kane,
+        "matches": "Liverpool",
+        "points": "50"
+    },
+    {
+        "team": "Man Utd",
+        "players":Memphis Depay,
+        "matches": "West Ham",
+        "points": "45" 
+    },
+    {
+        "team": "Chelsea",
+        "players":Diego Costa,
+        "matches": "Norwich",
+        "points": "40" 
+    }
+    ];
+    
+});
